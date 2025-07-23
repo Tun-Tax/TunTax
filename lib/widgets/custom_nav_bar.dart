ba02b4559afe75fee36e45166e38f3eec3a043dd
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-07-23 21:06:22 +0700

    refactor(navigation): streamline CustomNavBar by extracting _navBarItems method for better readability

--- lib/widgets/custom_nav_bar.dart
+++ lib/widgets/custom_nav_bar.dart
@@ -13,34 +13,63 @@ class CustomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/svgs/home.svg'),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/svgs/file_text.svg'),
-          label: 'Transactions',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/images/navbar-logo.png'),
-          label: 'Chat',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/svgs/search_person.svg'),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/svgs/person.svg'),
-          label: 'Account',
-        ),
-      ],
-      currentIndex: currentIndex,
-      onTap: onTap,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: _navBarItems(),
+        currentIndex: currentIndex,
+        onTap: onTap,
+      ),
     );
+  }
+
+  List<BottomNavigationBarItem> _navBarItems() {
+    final List<Map<String, dynamic>> items = [
+      {'iconPath': 'assets/svgs/home.svg', 'label': 'Home', 'isSvg': true},
+      {
+        'iconPath': 'assets/svgs/file_text.svg',
+        'label': 'Transactions',
+        'isSvg': true
+      },
+      {
+        'iconPath': 'assets/images/navbar-logo.png',
+        'label': 'Chat',
+        'isSvg': false
+      },
+      {
+        'iconPath': 'assets/svgs/search_person.svg',
+        'label': 'Search',
+        'isSvg': true
+      },
+      {
+        'iconPath': 'assets/svgs/person.svg',
+        'label': 'Account',
+        'isSvg': true
+      },
+    ];
+
+    return items.map((item) {
+      final icon = item['isSvg']
+          ? SvgPicture.asset(item['iconPath'])
+          : Image.asset(item['iconPath']);
+      return BottomNavigationBarItem(
+        icon: icon,
+        activeIcon: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+          child: icon,
+        ),
+        label: item['label'],
+      );
+    }).toList();
   }
 }
