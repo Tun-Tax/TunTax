d5a0b63906cd3aa53aef9d521ae8c6a422d90e67
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-07-23 20:53:50 +0700

    feat(navigation): implement CustomNavBar for improved navigation in ScaffoldWithNavBar

---
+++ lib/widgets/custom_nav_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+
+  const CustomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.swap_horiz),
+          label: 'Transactions',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Account',
+        ),
+      ],
+      currentIndex: currentIndex,
+      onTap: onTap,
+    );
+  }
+}
