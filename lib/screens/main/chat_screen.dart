339c7bac674bfaf8cd2e471a03cbeee2c726f270
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-08 21:56:35 +0700

    feat(chat): Enhance ChatScreen layout with ChatTextField and PromptChip widgets

--- lib/screens/main/chat_screen.dart
+++ lib/screens/main/chat_screen.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:tuntax/widgets/background_middle.dart';
+import 'package:tuntax/widgets/chat_text_field.dart';
+import 'package:tuntax/widgets/prompt_chip.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BackgroundMiddle(
+    return BackgroundMiddle(
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Text('Chat Screen'),
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Halo,\nbagaimana kami bisa membantu?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 32),
+                ChatTextField(),
+                SizedBox(height: 24),
+                PromptChip(text: 'Tata cara pelaporan pajak UMKM'),
+                SizedBox(height: 16),
+                PromptChip(text: 'Berita terbaru tentang insentif pemerintah'),
+                SizedBox(height: 16),
+                PromptChip(text: 'Bingung dengan fitur-fitur TunTax'),
+              ],
+            ),
+          ),
         ),
       ),
     );
