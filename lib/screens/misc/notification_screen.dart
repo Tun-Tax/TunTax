7f399e88bc5b19cf1e4fdb7d84025752e3247182
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-08 22:42:50 +0700

    feat(screens): Wrap DateScreen and NotificationScreen in Background widgets for improved layout

--- lib/screens/misc/notification_screen.dart
+++ lib/screens/misc/notification_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tuntax/widgets/background_alt.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Notification Screen')),
-      body: const Center(child: Text('This is the Notification Screen')),
+    return BackgroundAlt(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text('Notification Screen'),
+        ),
+        body: const Center(child: Text('This is the Notification Screen')),
+      ),
     );
   }
 }
