ab48bda73e3c4f355f6b6cb226e0470aa75b0fbf
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-08 21:40:11 +0700

    feat(appbar): Enhance CustomAppBar with improved styling and icon updates

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -8,16 +8,21 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Image.asset('assets/images/appbar-logo.png'),
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Image.asset(
+        'assets/images/appbar-logo.png',
+        height: 24,
+      ),
       actions: _appBarActions(),
     );
   }
 
   List<Widget> _appBarActions() {
     final List<Map<String, dynamic>> items = [
-      {'icon': Icons.calendar_month_outlined, 'label': 'Date'},
-      {'icon': Icons.notifications_none_rounded, 'label': 'Notification'},
-      {'icon': Icons.settings, 'label': 'Settings'},
+      {'icon': Icons.calendar_today_outlined, 'label': 'Date'},
+      {'icon': Icons.notifications_outlined, 'label': 'Notification'},
+      {'icon': Icons.settings_outlined, 'label': 'Settings'},
     ];
 
     return items
@@ -32,7 +37,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   Widget _buildAppBarIcon(IconData iconData, String label) {
     return IconButton(
-      icon: Icon(iconData, size: 24),
+      icon: Icon(iconData, size: 24, color: const Color(0xFF5D5D5D)),
       onPressed: () {
         onIconPressed?.call(label);
       },
