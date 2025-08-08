0a55a4024514aa61d31ca44a7745a1f05ef9a832
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-08 21:39:58 +0700

    feat(home): Wrap HomeScreen content with ScaffoldWithAppBar for improved layout

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tuntax/widgets/background.dart';
+import 'package:tuntax/widgets/scaffold_with_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +8,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Background(
-      child: Center(
-        child: Text('Home Screen'),
+      child: ScaffoldWithAppBar(
+        child: Center(
+          child: Text('Home Screen'),
+        ),
       ),
     );
   }
