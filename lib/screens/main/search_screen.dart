10fdb16318e0f07a6e715e6a178bd23a91ad360c
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-10 16:06:50 +0700

    refactor(search): Replace CustomAppBar with ScaffoldWithAppBar and update layout

--- lib/screens/main/search_screen.dart
+++ lib/screens/main/search_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tuntax/widgets/background.dart';
-import 'package:tuntax/widgets/custom_app_bar.dart';
+import 'package:tuntax/widgets/scaffold_with_app_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -8,12 +8,25 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Background(
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: CustomAppBar(),
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Text('Search Screen'),
+      child: ScaffoldWithAppBar(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Cari konsultan pajak tepercaya di dekatmu',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black, // Or appropriate text color
+                ),
+              ),
+              SizedBox(height: 16), // Add some space
+              Center(
+                child: Text('Search Screen'),
+              ),
+            ],
+          ),
         ),
       ),
     );
